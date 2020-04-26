db7c2ab310d1c08c3fb3d6cd237dddb6d63faf0f
Shyngys <Shyngys>
2020-04-26 23:04:51 +0600

    Initial Commit

---
+++ Lab_Ios_12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab_Ios_12
+//
+//  Created by Шынгыс on 4/26/20.
+//  Copyright © 2020 Шынгыс. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
