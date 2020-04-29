5377242bc99d821d4ee4625ddd503057bd9fc973
Shyngys <Shyngys>
2020-04-29 23:24:27 +0600

    fist

---
+++ Lab_Ios_12/AddMusicViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddMusicViewController.swift
+//  Lab_Ios_12
+//
+//  Created by Шынгыс on 4/29/20.
+//  Copyright © 2020 Шынгыс. All rights reserved.
+//
+
+import UIKit
+
+class AddMusicViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Lab_Ios_12/Track.swift
@@ -0,0 +1,9 @@
+//
+//  Track.swift
+//  Lab_Ios_12
+//
+//  Created by Шынгыс on 4/29/20.
+//  Copyright © 2020 Шынгыс. All rights reserved.
+//
+
+import Foundation
